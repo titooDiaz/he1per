0f3290c458510df00b4804dd8b9d1602497f7982
titooDiaz <miguelitodiaz169@gmail.com>
2025-11-21 13:56:49 -0500

    avoid worng readings

---
+++ README.md
@@ -0,0 +1,4 @@
+#pragma once
+
+#define WIFI_SSID "name"
+#define WIFI_PASS "passwords"
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,37 @@
+#include <Arduino.h>
 #include <WiFi.h>
+#include "secrets.h"
 
-const char* ssid = WIFI_SSID;
-const char* password = WIFI_PASS;
+// you can adjust this value to your needs
+// 0.1 = very stable but slow
+// 0.2 = faster but less filtered
+float alpha = 0.12;  
+
+float filteredRSSI = 0;
 
 void setup() {
   Serial.begin(115200);
 
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   Serial.print("Conectando");
   while (WiFi.status() != WL_CONNECTED) {
     delay(200);
     Serial.print(".");
   }
-
   Serial.println("\nConectado!");
+  filteredRSSI = WiFi.RSSI();
 }
 
 void loop() {
-  int rssi = WiFi.RSSI();
-  Serial.print("RSSI: ");
-  Serial.println(rssi);
+  int rawRSSI = WiFi.RSSI();
+  filteredRSSI = alpha * rawRSSI + (1 - alpha) * filteredRSSI;
+
+  Serial.print("RSSI crudo: ");
+  Serial.print(rawRSSI);
+  Serial.print("  |  RSSI filtrado: ");
+  Serial.println(filteredRSSI);
 
   delay(150);
 }
